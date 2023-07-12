136c6582105ed12ca107d5004ff9ce96f65ec508
showkhin1997 <showkhin15@gmail.com>
2023-07-12 12:12:03 +0600

    countries

--- js/countries.js
+++ js/countries.js
@@ -8,15 +8,43 @@ loadCountries();
 const displayCountries = countries => {
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country);
+
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
+        div.innerHTML = `
+            <h3>${country.name.common}</h3>
+            <p>${country.capital}</p>
+            <button onClick="loadCountryName('${country.name.common}')">Details</button>
+        `
+        /* const h3 = document.createElement('h3');
         h3.innerText = country.name.common;
         const p = document.createElement('p');
         p.innerText = country.capital;
         div.appendChild(h3);
-        div.appendChild(p);
+        div.appendChild(p); */
+
         countriesDiv.appendChild(div);
     })
+}
+
+
+const loadCountryName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => countryDetails(data[0]))
+}
+
+const countryDetails = details => {
+    console.log(details);
+    const countryDiv = document.getElementById('country-details');
+    countryDiv.classList.add('country-details');
+    countryDiv.innerHTML = `
+    <h3>Country Name: ${details.name.common}</h3>
+    <h4>Capital: ${details.capital}</h4>
+    <h5>Region: ${details.region}</h5>
+    <p>Time Zone: ${details.timezones}</p>
+    <p>Population: ${details.population}</p>
+    <img width='30%' src='${details.flags.png}'>
+`
 }
